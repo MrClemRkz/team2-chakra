f7cc88e638132643c2efe47bbc24ecc013754069
Mohamed Nizar <nizarucsc@gmail.com>
2020-02-07 13:46:12 +0530

    Add back addition method

--- calculator.java
+++ calculator.java
@@ -1,3 +1,28 @@
 // this will be the main class file
 
 // TODO: will have seperate classes for each functionality.
+
+import java.util.Scanner;
+
+class Add
+{
+    public static void main(String arg[])
+
+    {
+
+        int n;double res=0;
+
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter number you want to calc");
+
+        int sum = 0;
+        while (sc.hasNext()){
+            sum += sc.nextInt();
+            System.out.println("Sum = :" + sum);
+        }
+        System.out.println("Sum of numbers= :"+sum);
+
+    }
+
+}
